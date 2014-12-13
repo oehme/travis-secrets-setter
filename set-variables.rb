be4c8034c815009a5ed1ee55b50810a7f5752f09
Stefan Oehme <st.oehme@gmail.com>
2014-12-13 22:08:52 +0100

    some logging

--- set-variables.rb
+++ set-variables.rb
@@ -4,6 +4,7 @@ repos = Travis::Repository.find_all(owner_name: 'oehme')
 keys = ['ORG_GRADLE_PROJECT_bintrayApiKey', 'ORG_GRADLE_PROJECT_signingPassword', 'ORG_GRADLE_PROJECT_sonatypePassword']
 repos.each do |repo|
 	keys.each do |key|
+		puts "Setting env var '#{key}' on project '#{repo.slug}'"
 		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
 	end
 end
