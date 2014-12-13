2145b9750305c4444f8e849af111136cf7209569
Stefan Oehme <st.oehme@gmail.com>
2014-12-13 22:01:15 +0100

    fixed typo

--- set-variables.rb
+++ set-variables.rb
@@ -1,5 +1,5 @@
 require 'travis'
-Travis.access_token = travis.github_auth(ENV['TRAVIS_TOKEN'])
+Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN'])
 repos = Travis::Repository.find_all(owner_name: 'oehme')
 keys = ['ORG_GRADLE_PROJECT_bintrayApiKey', 'ORG_GRADLE_PROJECT_signingPassword', 'ORG_GRADLE_PROJECT_sonatypePassword']
 repos.each do |repo|
