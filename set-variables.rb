03e8c202f7774f345ab123ee01b4a91d495be1e6
Stefan Oehme <st.oehme@gmail.com>
2014-12-13 22:06:23 +0100

    double quote variables

--- set-variables.rb
+++ set-variables.rb
@@ -4,6 +4,6 @@ repos = Travis::Repository.find_all(owner_name: 'oehme')
 keys = ['ORG_GRADLE_PROJECT_bintrayApiKey', 'ORG_GRADLE_PROJECT_signingPassword', 'ORG_GRADLE_PROJECT_sonatypePassword']
 repos.each do |repo|
 	keys.each do |key|
-		repo.env_vars.upsert(key, "#{ENV[key]}", public: false)
+		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
 	end
 end
